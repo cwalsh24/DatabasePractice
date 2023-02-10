4e5f8264dd04bdae079dd01c5caaec6f380fa8e8
cwalsh24 <cwalsh24@ut.utm.edu>
2023-02-10 12:02:59 -0600

    Current version, working on the program

--- DatabasePractice/MainWindow.xaml.cs
+++ DatabasePractice/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using System.Data.OleDb;
 
 namespace DatabasePractice
 {
@@ -22,7 +24,26 @@ namespace DatabasePractice
     {
         public MainWindow()
         {
+            OleDbConnection cn; 
+            cn = new OleDbConnection("SampleDatabase1ConnectionString");
             InitializeComponent();
+        }
+        private void AssetButton(object sender, RoutedEventArgs e)
+        {
+            string query = "select* from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += read[0].ToString() + "\n";
+
+            }
+
+            //Now display data in the Text attribute of your TextArea.
+            //Modify your code so you can display all fields of the Assets table
+            //Modify your program so you have a similar button that will display contents of Employees table
         }
     }
 }
